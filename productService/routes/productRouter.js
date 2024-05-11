c88870ef6f33080faaba9dcfec8fe4ad4e725e60
tharinduthilakarathna98 <it20604844@my.sliit.lk>
2024-05-11 13:15:42 +0530

    build latest

--- productService/routes/productRouter.js
+++ productService/routes/productRouter.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.get('/', productController.getProducts);
 
 router.get('/:idOrName', productController.findProduct);
-
+//add comment
 router.post('/', productController.createProduct);
 
 module.exports = router;
